7f58191dfb98537e243b792f6b6b5e0848f4c3fc
arnort20 <arnort20@ru.is>
2023-03-11 10:50:09 +0000

    fresh start

---
+++ script.js
@@ -0,0 +1,5 @@
+<Object data ="CV_ENG.pdf" type="application/pdf" width="100%" height="100%">
+    <p>
+        Error message <a href="CV_ENG.pdf">click here to download the pdf file</a>
+    </p>
+</Object>
